d6bead5876dbbe63f1c37d862110bc4595aaa662
Asilbek Sadiyev <kvaradonauz@gmail.com>
2026-01-14 16:39:44 +0500

    added some styles, still far away from being ready

--- project-module/src/main.tsx
+++ project-module/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { NavigateApp } from './react-components/Navigation'
-import {RegisterApp} from "./react-components/registration/Register"
+import {RegisterApp} from "./react-components/registration/LoginIn"
 import "./styles.scss";
 
 createRoot(document.getElementById('root')!).render(
---
+++ project-module/src/react-components/registration/LoginIn.tsx
@@ -0,0 +1,57 @@
+export const RegisterApp = () => {
+  return (
+    <div className="register_panel">
+      <div className="register_wrapper">
+        <svg className="register_panel-icon" width="30" height="30">
+          <use href="./public/img/file.svg#market-icon"></use>
+        </svg>
+        <h1 className="register_panel-title">Welcome Back</h1>
+        <p className="register_panel-description">
+          Sign in to your account to continue shopping
+        </p>
+      </div>
+      <form className="register_form">
+        <div className="register_form-inner">
+          <span className="register_form-title">Email</span>
+          <label className="register_form-label">
+            <svg width="20" height="20">
+              <use href="./public/img/file.svg#email-icon"></use>
+            </svg>
+            <input
+              className="register_form-input"
+              placeholder="your@example.com"
+            ></input>
+          </label>
+        </div>
+        <div className="register_form-inner">
+          <span className="register_form-title">Password</span>
+          <label className="register_form-label">
+            <svg className="register_form-icon" width="20" height="20">
+              <use xlinkHref="./public/img/file.svg#lock-icon"></use>
+            </svg>
+
+            <svg className="register_form-icon" width="20" height="20">
+              <use xlinkHref="./public/img/file.svg#eye-icon"></use>
+            </svg>
+            <input
+              className="register_form-input"
+              placeholder="&#8901;&#8901;&#8901;&#8901;&#8901;&#8901;&#8901;&#8901;"
+            ></input>
+          </label>
+        </div>
+
+        <div className="register_form-inner">
+          <a className="register_form-recover">Forgot password?</a>
+          <button className="register_form-submit">Sign In</button>
+        </div>
+
+        <div className="register_form-inner">
+          <span className="register_form-login-title">
+            Don't have an account?
+          </span>
+          <span className="register_form-login">Sign Up</span>
+        </div>
+      </form>
+    </div>
+  );
+};
