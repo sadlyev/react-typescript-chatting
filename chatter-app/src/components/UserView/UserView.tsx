726953f54f41f5c42ded92504c1662eb1ac4833a
Asilbek Sadiyev <kvaradonauz@gmail.com>
2026-02-03 01:23:13 +0500

    small chnages

--- chatter-app/src/components/UserView/UserView.tsx
+++ chatter-app/src/components/UserView/UserView.tsx
@@ -4,6 +4,6 @@ export const UserView = ({username} : {username: string}) => {
     const colors: string[] = ["red", "blue", "green", "orange"]
 
     return (
-            <span className="user_logo-letter" style={{backgroundColor: colors[Math.floor(Math.random() * colors.length)]}}>{username[0].toLocaleUpperCase()}</span>
+            <span className="user_logo-letter" style={{backgroundColor: colors[Math.floor(Math.random() * colors.length)]}}>{username?.[0]?.toLocaleUpperCase() || "?"}</span>
     )
 }
