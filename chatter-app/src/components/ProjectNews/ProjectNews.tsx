bbf0287851e80cdee18071da5250778b793806b6
Asilbek Sadiyev <kvaradonauz@gmail.com>
2026-02-12 15:21:49 +0500

    adding a news page

--- chatter-app/src/components/ProjectNews/ProjectNews.tsx
+++ chatter-app/src/components/ProjectNews/ProjectNews.tsx
@@ -3,6 +3,11 @@ import type {NewsType} from "../../TTypes/TTypes"
 export const ProjectNews = ({id, title, tags, text} : NewsType)=> {
     return (
         <div>
+            <span>{title}</span>
+            <ul>{tags.map((tag, i) => (
+                <li key={i}>{tag}</li>
+            ))}</ul>
+            <p>{text}</p>
 
         </div>
     )
