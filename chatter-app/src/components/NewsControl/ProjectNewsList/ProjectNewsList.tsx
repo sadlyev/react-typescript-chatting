27f682f290f05a819843d032ec5f9506bcf8bd07
Asilbek Sadiyev <kvaradonauz@gmail.com>
2026-02-16 22:38:11 +0500

    tested some new functionalities

--- chatter-app/src/components/NewsControl/ProjectNewsList/ProjectNewsList.tsx
+++ chatter-app/src/components/NewsControl/ProjectNewsList/ProjectNewsList.tsx
@@ -7,20 +7,29 @@ import { AppHeader } from "../../CommonComp/Appheader/AppHeader"
 export const NewsList = () => {
     const [searhParams, setSearchParams] = useSearchParams()
 
-    
+    function  handleSearch(e: any) {
+        const {value } = e.target
+
+        setSearchParams({search: value})
+    }
+
+    const search = searhParams.get("search") || ""
+
+    const filteredNews = NewsPost.filter((post) => post.title.toLowerCase().includes(search.toLocaleLowerCase()))
+  
 
     return (
         <div className="news">
             <AppHeader></AppHeader>
-            {NewsPost.map((post, i) => (
-                <Link to={`/news/${i}`}>{
+            <input onChange={handleSearch}></input>
+            {filteredNews.map((post, i) => (
+                <Link key={i} to={`/news/${i}`}>{
                     <div>
                         <p>{post.title}</p>
                         <p>{post.text}</p>
                     </div>
                 }</Link>
             ))}
-
         </div>
     )
 }
